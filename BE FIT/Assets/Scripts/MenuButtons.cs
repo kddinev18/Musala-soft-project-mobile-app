a60702a41875369cb29575e189563052d39e5a9b
kddinev18 <kddinev18@codingburgas.bg>
2021-07-07 11:53:48 +0300

    Finish the MainMenu

--- BE FIT/Assets/Scripts/MenuButtons.cs
+++ BE FIT/Assets/Scripts/MenuButtons.cs
@@ -6,19 +6,35 @@ using UnityEngine.SceneManagement;
 public class MenuButtons : MonoBehaviour
 {
     [SerializeField] private Animator startButton;
-    [SerializeField] private Animator SceneTrasition;
+    [SerializeField] private Animator sceneTrasition;
+    [SerializeField] private Animator menuAnimation;
     [SerializeField] private int index;
+    private bool isPressedBefore = false; 
 
     public void pressStart()
     {
         StartCoroutine(wait());
     }
 
+    public void pressMenu()
+    {
+        if (!isPressedBefore)
+        {
+            menuAnimation.SetBool("PressMenuButton", true);
+            isPressedBefore = true;
+        }
+        else if(isPressedBefore)
+        {
+            menuAnimation.SetBool("PressMenuButton", false);
+            isPressedBefore = false;
+        }
+    }
+
     IEnumerator wait()
     {
         startButton.SetBool("PressButton", true);
         yield return new WaitForSeconds(1f);
-        SceneTrasition.SetBool("EndScene", true);
+        sceneTrasition.SetBool("EndScene", true);
         yield return new WaitForSeconds(2f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + index);
     }
