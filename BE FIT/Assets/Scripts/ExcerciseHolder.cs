a6588d5d9f0e4f22fd82f2c1163b47d08f0d6d06
kddinev18 <kddinev18@codingburgas.bg>
2021-07-12 10:49:00 +0300

    Implement Data Files system

--- BE FIT/Assets/Scripts/ExcerciseHolder.cs
+++ BE FIT/Assets/Scripts/ExcerciseHolder.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Linq;
 
 public class ExcerciseHolder : MonoBehaviour
 {
-    public string[] excercises = { "jumping jacks", "plank", "push ups" };
-    public string[] description = { "1", "2", "3" };
+    public string[] excercises;
+    public string[] description;
+    public string[] time;
+
+    void Start()
+    {
+        excercises = File.ReadAllLines("Assets\\Data Files\\ExcerciseNameData.txt").ToArray();
+        description = File.ReadAllLines("Assets\\Data Files\\ExcerciseDescriptionData.txt").ToArray();
+        time = File.ReadAllLines("Assets\\Data Files\\ExcerciseTimeData.txt").ToArray();
+    }
+
 }
--- BE FIT/Assets/Scripts/GenerateExcercises.cs
+++ BE FIT/Assets/Scripts/GenerateExcercises.cs
@@ -4,83 +4,37 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GenerateExcercises : MonoBehaviour
-{
-    private float time = 31f;
-    private int timeContainer;
-    private int numberOfGenerations = 0;
-    private bool isStarted = false;
-    private int randomExcercise;
-    private int TrainingDone = 0;
-    
-    [SerializeField] private Text timeDesplay;
+{   
     [SerializeField] private ExcerciseHolder excerciseHolder;
     [SerializeField] private ExperienceSystem experienceSystem;
 
-    public Text excercisesNameDisplay;
-    public Text excercisesDescriptionDisplay;
-    
+    [SerializeField] private Text excercisesNameDisplay;
+    [SerializeField] private Text excercisesDescriptionDisplay;
+    [SerializeField] private Text timeDesplay;
     void Start()
     {
-        timeDesplay.enabled = false;
-        generate(excerciseHolder.excercises, excerciseHolder.description);
-        experienceSystem.levelCapacity += 50;
-        StartCoroutine(wait());
+        
     }
 
     void Update()
     {
-        if (isStarted)
-        {
-            if (TrainingDone != 4)
-            {
-                time -= Time.deltaTime;
-                timeContainer = (int)time;
-                timeDesplay.text = timeContainer.ToString();
-                if (time < 0)
-                {
-                    if (numberOfGenerations == 4)
-                    {
-                        timeDesplay.enabled = false;
-                        excercisesNameDisplay.text = "Rest";
-                        excercisesDescriptionDisplay.text = "Rest";
-                        numberOfGenerations++;
-                        isStarted = false;
-                        StartCoroutine(wait());
-                        time = 16f; 
-                        TrainingDone++;
-                        numberOfGenerations = 0;
-                    }
-                    else
-                    {
-                        timeDesplay.enabled = false;
-                        generate(excerciseHolder.excercises, excerciseHolder.description);
-                        time = 31f;
-                        numberOfGenerations++;
-                        isStarted = false;
-                        StartCoroutine(wait());
-                        experienceSystem.levelCapacity += 50;
-                    }
-                }
-            }
-            else
-            {
-                excercisesNameDisplay.text = "Training Complete";
-                excercisesDescriptionDisplay.text = "";
-            }
-        }
+       
     }
+
+    private void generateExcercise(string[] excerciseName, string[] excerciseDesc, string excerciseTime)
+    {
+        int randomindex = Random.Range(0f, 3f);
+        excercisesNameDisplay = excerciseName[randomindex];
+        excercisesDescriptionDisplay = excerciseDesc[randomindex];
+        timeDesplay = excerciseTime[randomindex];
+    }
+
+
     IEnumerator wait()
     {
         yield return new WaitForSeconds(3f);
         isStarted = true;
         yield return new WaitForSeconds(.1f);
         timeDesplay.enabled = true;
-    }
-
-    public void generate(string[] excercises, string[] description)
-    {
-        randomExcercise = Random.Range(0, 3);
-        excercisesNameDisplay.text = excercises[randomExcercise];
-        excercisesDescriptionDisplay.text = description[randomExcercise];
     }
 }
